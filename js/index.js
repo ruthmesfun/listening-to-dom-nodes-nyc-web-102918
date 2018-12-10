67b2dd536a58dc013d1ed0b6899ebdaba09156e0
ruthmesfun <ruth.mesfun@gmail.com>
2018-12-10 18:14:33 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,27 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const main = document.getElementById('main');
+
+main.addEventListener('click',event => alert('I was clicked!'));
+
+const input = document.querySelector('input');
+
+input.addEventListener('keydown', e => {
+    if (e.which === 71){
+        return e.preventDefault()
+    }else{
+        console.log(e.which)
+    }
+});
+
+let divs = document.querySelectorAll('div');
+
+
+// Why must it be this way rather than using the arrow function? 
+function  capture(e){
+    console.log(this.firstChild.nodeValue.trim() + 'captured');
+} 
+
+for (let i = 0; i < divs.length; i++){
+    divs[i].addEventListener('click',capture, true);
+} 
